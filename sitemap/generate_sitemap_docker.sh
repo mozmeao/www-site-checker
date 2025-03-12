057620c816837301516a62d9cc0a135266276d65
Paul McLanahan <pmac@mozilla.com>
2025-03-11 23:29:05 -0400

    Remove DMS ping

--- sitemap/generate_sitemap_docker.sh
+++ sitemap/generate_sitemap_docker.sh
@@ -5,17 +5,11 @@ set -exo pipefail
 cd sitemap
 
 docker build --pull \
-             --platform "linux/amd64" \
              -t sitemap-generator \
              --build-arg "USER_ID=$(id -u):$(id -g)" \
              .
 
 docker run --rm \
-           --platform "linux/amd64" \
            --env-file .bedrock.env \
            -v "$PWD:/app/sitemap-data" \
            sitemap-generator ./run-generator.sh
-
-if [[ -n "$SNITCH_URL" ]]; then
-    curl "$SNITCH_URL"
-fi
